81d1e07c070b3f62023a12215fc8cfc33ddbf537
Andrew Washburn <awashburn@its4logistics.com>
2025-06-19 10:24:42 -0700

    test pre commit

--- Freight.UI/src/app/app.ts
+++ Freight.UI/src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected title = 'Freight.UI';
+  protected title = 'Freight.UI'; variable = 'Freight.UI';
 }
